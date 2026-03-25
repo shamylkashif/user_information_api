3419f0abfd00292944eb78cda1bcc822c05f1a85
shamylkashif <144819710+shamylkashif@users.noreply.github.com>
2026-03-25 18:45:18 +0500

    learned about debugPrint statement.

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,7 +4,7 @@ import 'package:http/http.dart' as http;
 class ApiService {
   Future<List<UserModel>> fetchUsers()async {
     final url = Uri.parse('https://jsonplaceholder.typicode.com/users');
-    print("Fetching users from URL: $url");
+    debugPrint("Fetching users from URL: $url");
     final response = await http.get(
      url,
      headers: {
@@ -12,15 +12,15 @@ class ApiService {
        'User-Agent' : 'FlutterApp',
      },
     );
-    print("Response status code: ${response.statusCode}");
-    print("response body: ${response.body}");
+    debugPrint("Response status code: ${response.statusCode}");
+    debugPrint("response body: ${response.body}");
 
     if(response.statusCode == 200){
       final List jsonData = json.decode(response.body);
-      print("Decoded JSON length: ${jsonData.length}");
+      debugPrint("Decoded JSON length: ${jsonData.length}");
       return jsonData.map((json) => UserModel.fromJson(json)).toList();
     } else {
-       print("Failed to load users");
+       debugPrint("Failed to load users");
        throw Exception("Failed to load users");
     }
   }
