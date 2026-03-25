3e24e5ef03502c46179600d457f28435dd46172b
shamylkashif <144819710+shamylkashif@users.noreply.github.com>
2026-03-25 11:26:30 +0500

    Added all related files

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,12 @@
+import 'package:userinformationapi/utils/app_imports.dart';
+
+class LoginScreen extends StatelessWidget {
+  const LoginScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
---
+++ lib/utils/app_imports.dart
@@ -0,0 +1,5 @@
+export 'package:userinformationapi/main.dart';
+export 'package:userinformationapi/screens/login_screen.dart';
+export 'package:get/get.dart';
+export 'package:flutter/material.dart';
+export 'package:userinformationapi/utils/app_imports.dart';
